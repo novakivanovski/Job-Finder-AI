61c118e54d31a6d542469734829fd4be03e0e5c5
Novak <novakivanovski@gmail.com>
2018-06-29 23:30:05 -0400

    Storage class functionality added

--- Storage.py
+++ Storage.py
@@ -2,13 +2,19 @@ import os  # put some of the IOUtils stuff here - split into networking and loca
 import pickle
 import logging
 from nltk import word_tokenize
+from enum import Enum
+
+
+class Path(Enum):
+    FILE = 0
+    FOLDER = 1
 
 
 class Storage:
     def __init__(self, base_directory):
         self.base_dir = base_directory
         self.jobs_dir = os.path.join(base_directory, 'jobs')
-        self.train_dir = os.path.join(self.jobs_dir, 'training')
+        self.train_dir = os.path.join(base_directory, 'training')
         self.train_pass_dir = os.path.join(self.train_dir, 'pass')
         self.train_fail_dir = os.path.join(self.train_dir, 'fail')
         self.classify_dir = os.path.join(self.jobs_dir, 'classify')
@@ -21,10 +27,10 @@ class Storage:
 
     def store_job(self, job):
         job_dir = os.path.join(self.jobs_dir, job.metadata.id)
-        self.create_directory(job_dir)
         text_file_path = os.path.join(job_dir, self.job_file_name)
         object_file_path = os.path.join(job_dir, self.job_object_name)
-        self.store_text(text_file_path, job.text)
+        self.create_directory(job_dir)
+        self.store_job_text(text_file_path, job)
         self.store_object(object_file_path, job)
 
     @staticmethod
@@ -34,6 +40,29 @@ class Storage:
                 pickle.dump(obj, file)
         except Exception as e:
             logging.error('Error while pickling: ' + str(e))
+
+    def retrieve_jobs(self):
+        job_folders = self.get_folders_from_path(self.jobs_dir)
+        jobs = []
+        for folder in job_folders:
+            pickle_paths = self.get_pickles_in_folder(folder)
+            for pickle_path in pickle_paths:
+                job = self.retrieve_object(pickle_path)
+                jobs.append(job)
+        return jobs
+
+    def get_pickles_in_folder(self, folder):
+        files = self.get_files_from_path(folder)
+        pickles = self.get_files_with_extension(files, extension='.pickle')
+        return pickles
+
+    @staticmethod
+    def get_files_with_extension(files_in, extension=''):
+        files_out = []
+        for file in files_in:
+            if file.endswith(extension):
+                files_out.append(file)
+        return files_out
 
     @staticmethod
     def retrieve_object(file_path):
@@ -45,13 +74,32 @@ class Storage:
                 logging.error('Error while retrieving object: ' + str(e))
         return obj
 
-    @staticmethod
-    def store_text(file_path, data):
+    def store_job_text(self, file_path, job):
         try:
             with open(file_path, 'w', encoding='utf-8') as file:
-                file.write(data)
+                self.dump_job_text(file, job)
         except Exception as e:
             logging.error('Error while storing text: ' + str(e))
+
+    def dump_job_text(self, file, job):
+        file.write(job.title + '\n')
+        for keyword in job.keywords:
+            file.write(keyword + ", ")
+            file.write('\n')
+        file.write(job.url + '\n')
+        file.write(job.location + '\n')
+        file.write(job.company + '\n')
+        file.write(job.date + '\n')
+        file.write('\n\n')
+        text = word_tokenize(job.raw)
+        self.write_formatted_text(file, text)
+
+    @staticmethod
+    def write_formatted_text(file, text, max_words_per_line=25):
+        for i, word in enumerate(text):
+            file.write(word + ' ')
+            if i % max_words_per_line == 0:
+                file.write('\n')
 
     @staticmethod
     def retrieve_text(file_path):
@@ -79,32 +127,54 @@ class Storage:
         if not os.path.exists(directory):
                 os.makedirs(directory)
 
+    def get_files_from_path(self, path):
+        return self.get_items_from_path(path, item_type=Path.FILE)
+
+    def get_folders_from_path(self, path):
+        return self.get_items_from_path(path, item_type=Path.FOLDER)
+
+
+    def get_items_from_path(self, path, item_type=Path.FILE):
+        items = []
+        contents = self.get_contents_of_path(path)
+        for path in contents:
+            if self.check_file_type(path, path_type=item_type):
+                items.append(path)
+        return items
+
     @staticmethod
-    def get_start_index(directory):
-        int_list = []
-        max_value = 0
+    def get_contents_of_path(path):
+        contents = []
         try:
-            for file in os.listdir(directory):
-                i = int (file[:-4])
-                int_list.append(i)
-            max_value = max(int_list)
+            for file in os.listdir(path):
+                absolute_path = os.path.join(path, file)
+                contents.append(absolute_path)
         except Exception as e:
             logging.error(e)
-        return max_value
+        return contents
 
-    def write_jobs(self, jobs, pass_dir, fail_dir):
-        start_pass = self.get_start_index(pass_dir)
-        start_fail = self.get_start_index(fail_dir)
-    
+    @staticmethod
+    def check_file_type(path, path_type=Path.FILE):
+        match = False
+        if path_type == Path.FILE:
+            match = os.path.isfile(path)
+        elif path_type == Path.FOLDER:
+            match = os.path.isdir(path)
+        else:
+            logging.error('Error determining file type: ' + str(path_type))
+        return match
+
+    @staticmethod
+    def write_training_data(self, jobs):
+        jobs_dir = self.jobs_dir
         for i, job in enumerate(jobs):
             if job.passed:
-                file_name = str(start_pass + i) + '.txt'
-                file_dir = os.path.join(pass_dir, file_name)
+                pass
             else:
-                file_name = str(start_fail + i) + '.txt'
-                file_dir = os.path.join(fail_dir, file_name)
-            file = open(file_dir, 'w', encoding='utf-8')
+                pass
+            file = open(jobs_dir, 'w', encoding='utf-8')
             file.write(job.title + '\n')
+
             for keyword in job.keywords:
                 file.write(keyword + ", ")
             file.write('\n')
@@ -134,3 +204,4 @@ class Storage:
                     keywords.remove(',')
                 keywords_list.append(keywords)
         return keywords_list
+
