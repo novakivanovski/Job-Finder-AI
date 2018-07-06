793c17b4d916013d33ba0073f29eb0aab512d6e2
Novak <novakivanovski@gmail.com>
2018-07-06 10:38:57 -0400

    New classes

---
+++ DescriptionCrawlerFactory.py
@@ -0,0 +1,49 @@
+from importlib import import_module
+
+
+class DescriptionCrawlerFactory:
+    def __init__(self):
+        self.indeed = 'ca.indeed.com'
+        self.crawlers = {'IndeedDescriptionCrawler': 'ca.indeed.com', 'TalgroupDescriptionCrawler': 'talgroup.net',
+                         'WorkdayDescriptionCrawler': 'myworkdayjobs.com', 'AerotekDescriptionCrawler': 'www.aplitrak.com',
+                         'GenericDescriptionCrawler': ''}
+        self.package_name = 'DescriptionCrawlers'
+
+    def get(self, job):
+        crawler_instance = None
+        for crawler_type in self.crawlers:
+            job_site = self.crawlers[crawler_type]
+            if self.is_match(job.get_entry_url(), job_site):
+                class_name = crawler_type
+                module_name = self.package_name + '.' + class_name
+                crawler_module = import_module(module_name)
+                crawler_class = getattr(crawler_module, class_name)
+                crawler_instance = crawler_class()
+        return crawler_instance
+
+    @staticmethod
+    def is_match(url, job_site):
+        return url.find(job_site) != -1
+
+
+'''''
+        indeed = job.url.find('ca.indeed.com') != -1
+        talgroup = job.url.find('talgroup.net') != -1
+        workday = job.url.find('myworkdayjobs.com') != -1
+        aerotek = job.url.find('www.aplitrak.com') != -1
+        smoothhiring = job.url.find('app.smoothhiring.com') != -1
+        rbc = job.url.find('jobs.rbc.com') != -1
+        brassring = job.url.find('krb-sjobs.brassring.com') != -1
+        eagle = job.url.find('jobs.eagleonline.com') != -1
+        taleo = job.url.find('taleo.net') != -1
+        david_aplin = job.url.find('www.aplin.com') != -1
+        adp = job.url.find('workforcenow.adp.com') != -1
+        jobdiva = job.url.find('jobdiva.com') != -1
+        recruitinginmotion = job.url.find('recruitinginmotion.com') != -1
+        webconnect = job.url.find('webconnect.sendouts.net') != -1
+        akamai = job.company.find('Akamai') != -1
+        ian_martin = job.url.find('careers.ianmartin.com') != -1
+        teksystems = job.url.find('www.teksystems.com') != -1
+        hire_google = job.url.find('hire.withgoogle.com') != -1
+        google = job.url.find('careers.google.com') != -1
+'''''
---
+++ QueueUnpacker.py
@@ -0,0 +1,39 @@
+import logging
+from bs4 import BeautifulSoup
+
+
+class QueueUnpacker:
+    @staticmethod
+    def unpack(queue):
+        items = []
+        while not queue.empty():
+            try:
+                queue_item = queue.get()
+                items.append(queue_item)
+            except Exception as e:
+                logging.error('Error unpacking queue: ' + str(e))
+        return items
+
+    @staticmethod
+    def unpack_to_soup(queue):
+        html_texts = QueueUnpacker.unpack(queue)
+        soups = []
+        for text in html_texts:
+            soup = BeautifulSoup(text, 'html.parser')
+            soups.append(soup)
+        return soups
+
+
+    @staticmethod
+    def unpack_metadata(metadata_queue):
+        metadata_lists = QueueUnpacker.unpack(metadata_queue)
+        metadata = QueueUnpacker.flatten_list(metadata_lists)
+        return metadata
+
+    @staticmethod
+    def flatten_list(lists):
+        flattened_list = []
+        for this_list in lists:
+            for this_item in this_list:
+                flattened_list.append(this_item)
+        return flattened_list
