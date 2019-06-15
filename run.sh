16569018392d3ebef06cc22d008c476fb12b443c
novakivanovski <novakivanovski@gmail.com>
2019-06-15 14:09:25 -0400

    Shell script to run docker.

---
+++ run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+containers=$(docker container ls -a -q)
+images=$(docker image ls -a -q)
+
+docker container kill $containers
+docker container rm $containers
+docker image rm $images
+
+read -p "Build/run new instance? [y/n] " run_app
+if [$run_app = y]
+then
+	docker build -t jobfinder .
+	docker run -d -p 5000:5000 jobfinder
+fi
+
+docker ps
+
+
